fdc381cc47adb3062e2e5c43bae320a466b4d659
Nicola R <nicola.rizzi89@hotmail.it>
2020-12-16 17:02:53 +0100

    Task
    Now we can add new task at the todo list.

---
+++ main.js
@@ -0,0 +1,46 @@
+/* Create una todo list usando VueJS.
+Funzionalitá:
+La nostra todo list avrá alcune tasks di default predefinite
+L'utente puó inserire nuove tasks
+Cliccando sulla "X" l'utente puó cancellare una task
+Se non ci sono piu task nella lista, mostrate un messaggio tipo "Nulla da fare"
+Quando l'utente inserisce una task ha due modi per salvarla: o preme il pulsante add o preme il taso Enter della tastiera.
+Attenzione: l'utente non deve inserire tasks vuote ma almeno un tot di caratteri.
+ */
+
+ let app = new Vue({
+    el:"#app",
+    data:{
+        inputs: [],
+    },
+    methods:{
+
+        // inserisco nuove tasks
+        add(){
+            text = document.querySelector('input').value;
+            this.inputs.push ({value : text});
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ });
